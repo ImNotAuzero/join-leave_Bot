725a285b2198914459d717fe61f1c1a3024b9ad7
ImNotAuzero <31421526+ImNotAuzero@users.noreply.github.com>
2020-05-15 23:37:32 +0100

    CompleteBot - Not tested

---
+++ modules/functions.js
@@ -0,0 +1,13 @@
+module.exports.channels = {
+    cache_findByID: function(member, id) {
+        let r = member.guild.channels.cache.find(c => c.id === id);
+        return r;
+    }
+}
+
+module.exports.roles = {
+    cache_findByID: function(member, id) {
+        let r = member.guild.roles.cache.find(a => a.id === id);
+        return r;
+    }
+}
